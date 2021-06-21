d5a6ae26325debbad239fa143ccf706f5d3e1ca8
aaditya_raj <theultimatearaj@gmail.com>
2021-06-20 20:12:57 -0700

    fixed security

--- src/index.js
+++ src/index.js
@@ -7,16 +7,6 @@ import firebase from "firebase/app";
 import configData from "./config.json"
 
 
-// firebase.initializeApp({
-//   apiKey: "AIzaSyD_dxaZ75fz-C96Lq1MoziqerFGTJbQC6k",
-//   authDomain: "cap-project-86c79.firebaseapp.com",
-//   projectId: "cap-project-86c79",
-//   storageBucket: "cap-project-86c79.appspot.com",
-//   messagingSenderId: "308193142608",
-//   appId: "1:308193142608:web:f35982c86c92ea8296ae55",
-//   measurementId: "G-1RW0FRKMTR"
-// })
-
 firebase.initializeApp({
   apiKey: configData.apiKey,
   authDomain: configData.authDomain,
